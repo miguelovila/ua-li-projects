7a19dd68bf129c382c8d6a8f77ded1989b9d8f95
Miguel Vila <miguelovila@ua.pt>
2022-06-12 15:02:28 +0100

    Committing ./project/client/js/refresher.js

---
+++ projeto_final/project/client/js/refresher.js
@@ -0,0 +1,8 @@
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || 
+                           ( typeof window.performance != "undefined" && 
+                                window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      window.location.reload();
+    }
+});
